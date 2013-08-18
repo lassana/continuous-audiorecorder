464a29bbc1f1f34befa66b87187b5796f5e79925
lassana <lassana.nd@gmail.com>
2013-08-18 14:15:32 +0300

    Move MainActivity class to package "activity".

--- continuos-audiorecorder/src/main/java/com/github/lassana/continuos_audiorecorder/activity/MainActivity.java
+++ continuos-audiorecorder/src/main/java/com/github/lassana/continuos_audiorecorder/activity/MainActivity.java
@@ -1,9 +1,11 @@
-package com.github.lassana.continuos_audiorecorder;
+package com.github.lassana.continuos_audiorecorder.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+
+import com.github.lassana.continuos_audiorecorder.R;
 
 public class MainActivity extends FragmentActivity {
 

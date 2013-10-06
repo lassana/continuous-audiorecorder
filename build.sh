cead24089fa395ce235ec5065cb9f63a984f2db0
lassana <lassana.nd@gmail.com>
2013-10-06 16:06:44 +0300

    Add 'build.sh' for Drone.io

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export ANDROID_SDK_TOOLS_VERSION=22.2.1
+export ANDROID_BUILD_TOOLS_VERSION=18.1
+export ANDROID_API_LEVEL=18
+
+PATH=$(echo $PATH | sed 's/\/opt\/android-sdk-linux//')
+
+# Download and unpack Android SDK
+wget https://dl.google.com/android/android-sdk_r$ANDROID_SDK_TOOLS_VERSION-linux.tgz -nv
+tar xzf android-sdk_r$ANDROID_SDK_TOOLS_VERSION-linux.tgz
+
+export ANDROID_HOME=$PWD/android-sdk-linux
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/build-tools
+
+# --filter for 'build-tools' not working, I don't know why. Installing by number ('2') works fine.
+echo yes | android update sdk --no-ui --force --filter 2,tool,android-$ANDROID_API_LEVEL,platform-tool,extra > /dev/null
+
+./gradlew assembleDebug
